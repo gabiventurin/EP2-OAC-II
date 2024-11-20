6ed068965d94fc8004eb05a517cb220581c102cb
juliaduboisas <jujubadubois@gmail.com>
2024-11-20 18:19:22 -0300

    Fixed function headings in readAndConvert.h

--- readAndConvert.h
+++ readAndConvert.h
@@ -1,14 +1,16 @@
+#include <stdio.h>
+
 /**
  * Function that reads a string and returns the number of '\n' in it
  */
-int getNumberOfLines(char* file){}
+int getNumberOfLines(FILE* fptr){}
 
 /**
  * Function that receives a file and returns a double array of all the numbers in it
  */
-void readFileAndConvertToArray(char* f){}
+void readFileAndConvertToArray(char* f, double* array){}
 
 /**
  * Function that receives a double array and its size and puts it into the file "output/Ytest.txt" 
  */
-void readFileAndConvertToArray(char* f, double* array){}
+void readFileAndConvertToArray(double* array, int arraySize){}
