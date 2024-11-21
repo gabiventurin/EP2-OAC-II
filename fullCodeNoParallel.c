6ed5d93115617261640221c0ec7adf93a9625223
juliaduboisas <jujubadubois@gmail.com>
2024-11-21 19:46:42 -0300

    Started .c file for joining all the codes into a coherent algorithm to execute the KNN
    Waiting for the needed KNN changes to occur in order to continue the code

---
+++ fullCodeNoParallel.c
@@ -0,0 +1,12 @@
+// stdio.h ALREADY INCLUDED
+#include <stdlib.h>
+#include <readAndConvert.h> // already includes stdio.h because the type FILE* is needed in a header
+#include <matrizes>
+
+int main(){
+    char file1 = "data/Xtest.txt";
+
+    
+
+    return 0;
+}
