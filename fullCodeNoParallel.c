18e387a919b018514080a30e4e02ca4f310b0006
juliaduboisas <jujubadubois@gmail.com>
2024-12-10 19:03:30 -0300

    Fixed code in order to repeat the code x times and mark the meanTime for the sequential code
    Paralel code still not tested

--- fullCodeNoParallel.c
+++ fullCodeNoParallel.c
@@ -18,58 +18,81 @@ int main(){
 
     double* arrayXtrain1 = readFileAndConvertToArray(xtrain1);
     double* arrayXtest1 = readFileAndConvertToArray(xtest1);
+
+    printf("Quantas iterações devem ser realizadas?\n>");
     
-    printf("Arquivos colocados nos arrays!\n Iniciando o cronômetro e criando as matrizes do X...\n");
+    int iteracoes;
 
-    double timeInit = (double) clock();
-    timeInit = timeInit/CLOCKS_PER_SEC;
-
-    // agora os arrays de xtrain1 e xtest1 estão, respectivamente, em arrayXtrain1 e arrayXtest1
-
-    // CRIACAO DAS MATRIZES
-    int nXtrain1 = getNumberOfLines(xtrain1);
-    int nXtest1 = getNumberOfLines(xtest1);
-
-    int h = 1;
-    int w = 3;
-
-    int rowsXtrain1 = nXtrain1 - w - h + 2;
-    int rowsXtest1 = nXtest1 - w - h + 2;
-
-    double matrixXtrain1[rowsXtrain1][w];
-    double matrixXtest1[rowsXtest1][w];
-
-    createMatriz(nXtrain1, h, w, arrayXtrain1, matrixXtrain1);
-    createMatriz(nXtest1, h, w, arrayXtest1, matrixXtest1);
-
-    printf("Matrizes do X criadas!\nCriando YTrain...\n");
-
-    // CRIACAO DO YTRAIN
-
-    int ytrain1Size = nXtrain1 - w - h + 1;
-    double* ytrain1 = (double*)malloc(ytrain1Size * sizeof(double));
+    scanf("%i", &iteracoes);
     
-    createYtrain(nXtrain1, h, w, ytrain1Size, arrayXtrain1, ytrain1);
+    printf("Arquivos colocados nos arrays!\nExecutando %i vezes...\n", iteracoes);
 
-    printf("YTrain criada! Realizando o KNN...\n");
+    double tempos[iteracoes];
 
-    // REALIZACAO DO KNN
+    for(int i = 0; i<iteracoes; i++){
+        tempos[i] = iteracoes;
+    }
 
-    double* ytest1 = knn(w, nXtrain1, nXtest1, matrixXtrain1, matrixXtest1, ytrain1);
+    for(int i = 0; i < iteracoes; i++){
 
-    double timeEnd = (double) clock();
-    timeEnd = timeEnd / CLOCKS_PER_SEC;
+        double timeInit = (double) clock();
+        timeInit = timeInit/CLOCKS_PER_SEC;
 
-    printf("KNN criado e tempo final marcado!\nCalculando o tempo total...\n");
+        // agora os arrays de xtrain1 e xtest1 estão, respectivamente, em arrayXtrain1 e arrayXtest1
 
-    // COLOCAÇÃO DOS RESULTADOS EM UM ARQUIVO
+        // CRIACAO DAS MATRIZES
+        int nXtrain1 = getNumberOfLines(xtrain1);
+        int nXtest1 = getNumberOfLines(xtest1);
 
-    double time = timeEnd - timeInit;
+        int h = 1;
+        int w = 3;
 
-    printf("Tempo total: %.6lf\nColocando os resultados em um arquivo...", time);
+        int rowsXtrain1 = nXtrain1 - w - h + 2;
+        int rowsXtest1 = nXtest1 - w - h + 2;
 
-    readArrayAndConvertToFile(ytest1, nXtest1);
+        double matrixXtrain1[rowsXtrain1][w];
+        double matrixXtest1[rowsXtest1][w];
 
-    printf("Colocado no arquivo!\nPrograma finalizado!\n");
+        createMatriz(nXtrain1, h, w, arrayXtrain1, matrixXtrain1);
+        createMatriz(nXtest1, h, w, arrayXtest1, matrixXtest1);
+
+        // CRIACAO DO YTRAIN
+
+        int ytrain1Size = nXtrain1 - w - h + 1;
+        double* ytrain1 = (double*)malloc(ytrain1Size * sizeof(double));
+        
+        createYtrain(nXtrain1, h, w, ytrain1Size, arrayXtrain1, ytrain1);
+
+        // REALIZACAO DO KNN
+
+        double* ytest1 = knn(w, nXtrain1, nXtest1, matrixXtrain1, matrixXtest1, ytrain1);
+
+        double timeEnd = (double) clock();
+        timeEnd = timeEnd / CLOCKS_PER_SEC;
+
+        // COLOCAÇÃO DOS RESULTADOS EM UM ARQUIVO E FREE'S
+
+        tempos[i] = timeEnd - timeInit;
+
+        readArrayAndConvertToFile(ytest1, nXtest1);
+    }
+    
+    printf("Tempo de cada iteração:\n");
+
+    for(int i = 0; i<iteracoes; i++){
+        printf("> %i: %.6lf s\n", i+1, tempos[i]);
+    }
+
+    printf("Calculando tempo médio em %i iterações...\n", iteracoes);
+
+    double meanTime = 0;
+
+    for(int i = 0; i<iteracoes; i++){
+        meanTime += tempos[i];
+    }
+    meanTime = meanTime/iteracoes;
+
+    printf("Tempo médio: %.6lf s\nExecução finalizada!\n", meanTime);
+
     return 0;
 }
